bc0fe3b0cf528c8c72a131d92460b123d7e0d5cb
William Leung <leungwk25@gmail.com>
2017-11-22 11:57:12 -0800

    Fixing commit

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -4,68 +4,68 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
-      console.log(localMediaStream);
-      video.src = window.URL.createObjectURL(localMediaStream);
-      video.play();
-    })
-    .catch(err => {
-      console.error(`OH NO!!!`, err);
-    });
+function getVideo(){
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(localMediaStream => {
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  })
+  .catch(err => {
+    console.error(`Oh Noo!!!`, err);
+  });
+
 }
 
-function paintToCanvas() {
+function paintToCanvas(){
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
 
-  return setInterval(() => {
+  return setInterval(()=>{
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
-    let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
-    // pixels = redEffect(pixels);
+    //take the pixels out
+    let pixels = ctx.getImageData(0 ,0 , width, height);
+    //to mess with them
+    //pixels = redEffect(pixels);
+    //pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
 
-    pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
+    //This enables a slow motion
+    //ctx.globalAlpha = 0.8;
 
-    // pixels = greenScreen(pixels);
-    // put them back
     ctx.putImageData(pixels, 0, 0);
+
   }, 16);
 }
-
-function takePhoto() {
-  // played the sound
+function takePhoto(){
   snap.currentTime = 0;
   snap.play();
 
-  // take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-  strip.insertBefore(link, strip.firsChild);
+  link.innerHTML = `<img src="${data}" alt="Bootiful Person"/>`;
+  //link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+
 }
 
-function redEffect(pixels) {
-  for(let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+function redEffect(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4){
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //r
+    pixels.data[i + 1] = pixels.data[i + 1] -50; //g
+    pixels.data[i + 2] = pixels.data[i + 2] *0.5; //b
   }
   return pixels;
 }
 
-function rgbSplit(pixels) {
-  for(let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+function rgbSplit(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4){
+    pixels.data[i - 150] = pixels.data[i + 0]; //r
+    pixels.data[i + 100] = pixels.data[i + 1]; //g
+    pixels.data[i - 550] = pixels.data[i + 2]; //b
   }
   return pixels;
 }
@@ -98,5 +98,4 @@ function greenScreen(pixels) {
 }
 
 getVideo();
-
 video.addEventListener('canplay', paintToCanvas);
